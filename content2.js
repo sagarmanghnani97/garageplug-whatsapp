67bf960789c0cb650c099bf1b988b613927c08d6
sagarmanghnani97 <sagar@garageplug.com>
2019-03-15 11:59:55 +0530

    added popup html

--- content2.js
+++
@@ -1,9 +0,0 @@
-alert("Hello 2 ");
-intervalCheck = setInterval(() => {
-    var sendWhatsapp = document.getElementsByClassName("_35EW6")[0];
-    if(sendWhatsapp){
-        console.log("Hello 2 ");
-        sendWhatsapp.click();
-        clearInterval(intervalCheck);
-    }
-}, 5000);
---
+++ popup.js
@@ -0,0 +1,4 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    window.open("https://web.whatsapp.com");
+})
